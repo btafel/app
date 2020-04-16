682d2010915aa2ca52457c3392a5219900df358b
Bryan Tafel <bryantafel@macbook-pro.lan>
2020-04-16 09:59:50 -0400

    move translations to external file

--- App.tsx
+++ App.tsx
@@ -24,31 +24,11 @@ import Layout from './constants/Layout';
 
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
+import { translations } from './assets/translations';
 
 // Set the key-value pairs for the different languages you want to support.
-i18n.translations = {
-  es: {
-    slide1:
-      'CoTrack necesita saber tu ubicación para avisarte en tiempo real si en tu recorrido estuviste en contacto cercano con alguna persona contagiada',
-    slide2:
-      'Si presentás síntomas o creés haber estado en contacto con alguien infectado, CoTrack te puede ayudar a realizar un evaluación y aconsejarte qué hacer según el resultado',
-    slide3:
-      'CoTrack te brinda información completa y confiable para ayudar a prevenir la infección, medidas para resguardarse e información útil y actualizada',
-    Next: 'Siguiente',
-    Prev: 'Anterior',
-    Done: 'Finalizar',
-    title_signup:
-      'Necesitamos algunos datos tuyos para poder realizar un evaluación más preciso y contactarte si necesitas ayuda.',
-  },
-  it: { 
-      slide1: 'CoTrack deve conoscere la tua posizione per avvisarti in tempo reale se durante il tour eri in stretto contatto con una persona infetta',
-      slide2: 'Se hai sintomi o pensi di essere stato in contatto con qualcuno infetto, CoTrack può aiutarti a effettuare una valutazione e consigliarti cosa fare in base al risultato',
-      slide3: 'CoTrack fornisce informazioni complete e affidabili per aiutare a prevenire le infezioni, misure per proteggersi e informazioni utili e aggiornate',
-      Next: 'Seguente',
-      Prev: 'Anterior',
-      Done: 'Finalizza',
-  },
-};
+i18n.translations = translations;
+
 i18n.locale = 'it'; //Localization.locale.startsWith('it') ? 'it' : 'es';
 // Set the locale once at the beginning of your app.
 // i18n.locale = Localization.locale;
---
+++ assets/translations.ts
@@ -0,0 +1,26 @@
+export const translations = {
+  es: {
+    slide1:
+      'CoTrack necesita saber tu ubicación para avisarte en tiempo real si en tu recorrido estuviste en contacto cercano con alguna persona contagiada',
+    slide2:
+      'Si presentás síntomas o creés haber estado en contacto con alguien infectado, CoTrack te puede ayudar a realizar un evaluación y aconsejarte qué hacer según el resultado',
+    slide3:
+      'CoTrack te brinda información completa y confiable para ayudar a prevenir la infección, medidas para resguardarse e información útil y actualizada',
+    Next: 'Siguiente',
+    Prev: 'Anterior',
+    Done: 'Finalizar',
+    title_signup:
+      'Necesitamos algunos datos tuyos para poder realizar un evaluación más preciso y contactarte si necesitas ayuda.',
+  },
+  it: {
+    slide1:
+      'CoTrack deve conoscere la tua posizione per avvisarti in tempo reale se durante il tour eri in stretto contatto con una persona infetta',
+    slide2:
+      'Se hai sintomi o pensi di essere stato in contatto con qualcuno infetto, CoTrack può aiutarti a effettuare una valutazione e consigliarti cosa fare in base al risultato',
+    slide3:
+      'CoTrack fornisce informazioni complete e affidabili per aiutare a prevenire le infezioni, misure per proteggersi e informazioni utili e aggiornate',
+    Next: 'Seguente',
+    Prev: 'Anterior',
+    Done: 'Finalizza',
+  },
+};
