c39fe978a6296fd4d71c3f68dee957a6e51cc980
Joel A. Chornik <joel@latincloud.com>
2020-04-16 13:28:12 -0300

    Update translations.ts

--- assets/translations.ts
+++ assets/translations.ts
@@ -11,6 +11,7 @@ export const translations = {
     Done: 'Finalizar',
     title_signup:
       'Necesitamos algunos datos tuyos para poder realizar un evaluación más preciso y contactarte si necesitas ayuda.',
+    MakeTest: 'REALIZAR EVALUACION',
     MakeTestAgain: 'Realizar evaluación nuevamente',
     
     LowRisk: 'RIESGO LEVE',
@@ -26,7 +27,34 @@ export const translations = {
     HighRisk_subtitle: 'Te aconsejamos consultar con un profesional de acuerdo a las indicaciones en tu ciudad.',
     HighRisk_text: 'Aquí podés conseguir ayuda para conseguir los números de organismos oficiales según tu zona, para orientarte sobre cómo proceder y recibir asistencia médica y psicológica.',
 
+    AskAge: '¿Cuál es tu Edad?',
+    fever: 'Fiebre',
+    cough: 'Tos Seca',
+    throat: 'Dolor de Garganta',
+    breath: 'Dificultad para Respirar',
+    headache: 'Dolor de Cabeza',
+    diarrhea: 'Descompostura o diarrea',
+    tiredness: 'Cansancio general',
+    temperature: 'Temperatura corporal',
+    Contact_section: 'Viajes o Contactos Confirmados',
+    confirmedContact_subtitle: '¿Estuviste en contacto con algún caso confirmado de Coronavirus, en los ultimos 14 días?',
+    suspectedOutside_subtitle: '¿Estuviste de viaje fuera del país, en los ultimos 14 días?',
+    suspectedInside_subtitle: '¿Estuviste en alguna Provincia con casos locales de Coronavirus, en los ultimos 14 días?',
+    medicalHistory: 'Antecedentes Medicos',
+    inmunosuppresion: 'Inmunosupresión',
+    diabetes: 'Diabetes',
+    cancer: 'Cáncer',
+    hepatic: 'Enfermedad hepática',
+    pregnant: 'Embarazo',
+    newborn: 'Recién nacido con síntomas',
+    respiratoryDisease: 'Enfermedad respiratoria (Asma, EPOC)',
+    kidneyDisease: 'Enfermedad Renal Crónica',
+    cardiologicalDisease: 'Enfermedad cardiológica (HTA)',
+
   },
+
+
+  
   it: {
     slide1:
       'CoTrack deve conoscere la tua posizione per avvisarti in tempo reale se durante il tour eri in stretto contatto con una persona infetta',
@@ -38,18 +66,43 @@ export const translations = {
     Prev: 'Anterior',
     Done: 'Finalizza',
 
+    MakeTest: 'VALUTAZIONE DELLE PRESTAZIONI',
     MakeTestAgain: 'Eseguire di nuovo la valutazione',
     LowRisk: 'LEGGERO RISCHIO',
     MediumRisk: 'RISCHIO MODERATO',
     HighRisk: 'A RISCHIO',
 
-    LowRisk_subtitle: 'Según pautas oficiales, el conjunto de tus sintomas e historia no implican contagio de coronavirus o alto riesgo.',
-    LosRisk_text: 'Te proponemos realizar el aislamiento voluntario, repasando el listado de medidas preventivas y compartir esta información con tus allegados para así todos poder evitar el contagio',
+    LowRisk_subtitle: 'Secondo le linee guida ufficiali, l\'insieme dei sintomi e della storia non implica contagio del coronavirus o rischio elevato.',
+    LosRisk_text: 'Ti proponiamo di eseguire l\'isolamento volontario, rivedendo l\'elenco delle misure preventive e condividendo queste informazioni con i tuoi cari in modo che tutti possano evitare il contagio',
 
-    MediumRisk_subtitle: 'Algunos de tus síntomas pueden estar asociados al contagio de coronavirus pero no son concluyentes para determinar si efectivamente estás infectado.',
-    MediumRisk_text: 'Te proponemos realizar el aislamiento voluntario, repasando el listado de medidas preventivas y compartir esta información con tus allegados para así todos poder evitar el contagio',
+    MediumRisk_subtitle: 'Alcuni dei tuoi sintomi possono essere associati alla diffusione del coronavirus ma non sono conclusivi per determinare se sei effettivamente infetto.',
+    MediumRisk_text: 'Ti proponiamo di eseguire l\'isolamento volontario, rivedendo l\'elenco delle misure preventive e condividendo queste informazioni con i tuoi cari in modo che tutti possano evitare il contagio',
 
-    HighRisk_subtitle: 'Te aconsejamos consultar con un profesional de acuerdo a las indicaciones en tu ciudad.',
-    HighRisk_text: 'Aquí podés conseguir ayuda para conseguir los números de organismos oficiales según tu zona, para orientarte sobre cómo proceder y recibir asistencia médica y psicológica.',
+    HighRisk_subtitle: 'Ti consigliamo di consultare un professionista secondo le indicazioni nella tua città.',
+    HighRisk_text: 'Qui puoi ottenere aiuto per ottenere il numero di organizzazioni ufficiali in base alla tua area, per guidarti su come procedere e ricevere assistenza medica e psicologica.',
+
+    AskAge: '¿Qual è la tua età?',
+    fever: 'Febbre',
+    cough: 'Tosse secca',
+    throat: 'Mal di gola',
+    breath: 'Difficoltà a respirare',
+    headache: 'mal di testa',
+    diarrhea: 'Descompostura o diarrea',
+    tiredness: 'Stanchezza generale',
+    temperature: 'Temperatura corporea',
+    Contact_section: 'Contatti di viaggio o confermati',
+    confirmedContact_subtitle: 'Sei stato in contatto con casi confermati di Coronavirus negli ultimi 14 giorni?',
+    suspectedOutside_subtitle: 'Hai viaggiato fuori dal paese negli ultimi 14 giorni?',
+    suspectedInside_subtitle: 'Eri in una Provincia con casi locali di Coronavirus, negli ultimi 14 giorni?',
+    medicalHistory: 'Sfondo medico',
+    inmunosuppresion: 'immunosoppressione',
+    diabetes: 'diabete',
+    cancer: 'cancro',
+    hepatic: 'malattia del fegato',
+    pregnant: 'gravidanza',
+    newborn: 'Neonato con sintomi',
+    respiratoryDisease: 'Malattia respiratoria (Asma, EPOC)',
+    kidneyDisease: 'Malattia renale cronica',
+    cardiologicalDisease: 'Malattia cardiologica (HT)',
   },
 };
