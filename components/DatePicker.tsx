f74fce9cdd390bbde32f7936ef05fedda65b5808
Bryan Tafel <bryantafel@macbook-pro.lan>
2020-04-18 08:23:48 -0400

    fix default value (android)

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -5,25 +5,40 @@ import Colors from '../constants/Colors';
 export default function DatePicker({ label, onChange, value }) {
   // const dobRef = useRef<any | undefined>();
 
-  let defaultValue = {};
+  let defaultValue = { dobDay: 0, dobMonth: 0, dobYear: 0 };
 
   if (value) {
     const v = value.split('/');
     defaultValue = {
-      dobDay: v[0],
-      dobMonth: v[1],
-      dobYear: v[2],
+      dobDay: parseInt(v[0]),
+      dobMonth: parseInt(v[1]),
+      dobYear: parseInt(v[2]),
     };
   }
 
-  const [internalValue, setInternalValue] = useState(defaultValue);
+  // const [internalValue, setInternalValue] = useState(defaultValue);
+  const [internalDay, setInternalDay] = useState(defaultValue.dobDay);
+  const [internalMonth, setInternalMonth] = useState(defaultValue.dobMonth);
+  const [internalYear, setInternalYear] = useState(defaultValue.dobYear);
 
-  const handleChange = key => val => {
-    internalValue[key] = val;
-    setInternalValue(internalValue);
-    onChange(
-      `${internalValue.dobDay}/${internalValue.dobMonth}/${internalValue.dobYear}`,
-    );
+  const handleChange = (key) => (val) => {
+    // internalValue[key] = val;
+    // setInternalValue(internalValue);
+    console.log(key, val);
+    switch (key) {
+      case 'dobDay':
+        setInternalDay(val);
+        onChange(`${val}/${internalMonth}/${internalYear}`);
+        break;
+      case 'dobMonth':
+        setInternalMonth(val);
+        onChange(`${internalDay}/${val}/${internalYear}`);
+        break;
+      case 'dobYear':
+        setInternalYear(val);
+        onChange(`${internalDay}/${internalMonth}/${val}`);
+        break;
+    }
   };
 
   const arrDays = Array.from(Array(31).keys()).map((e, i) => (
@@ -45,7 +60,7 @@ export default function DatePicker({ label, onChange, value }) {
     'Diciembre',
   ].map((e, i) => <Picker.Item key={i + 1} label={e} value={i + 1} />);
 
-  const arrYears = Array.from(Array(100).keys()).map(i => (
+  const arrYears = Array.from(Array(100).keys()).map((i) => (
     <Picker.Item key={i + 1} label={`${2020 - i}`} value={2020 - i} />
   ));
 
@@ -82,7 +97,7 @@ export default function DatePicker({ label, onChange, value }) {
           )} */}
         <Picker
           // id="day"
-          selectedValue={internalValue.dobDay}
+          selectedValue={internalDay}
           onValueChange={handleChange('dobDay')}
           style={{ width: '30%' }}
           mode="dropdown"
@@ -92,7 +107,7 @@ export default function DatePicker({ label, onChange, value }) {
         </Picker>
         <Picker
           // id="month"
-          selectedValue={internalValue.dobMonth}
+          selectedValue={internalMonth}
           onValueChange={handleChange('dobMonth')}
           style={{ width: '30%' }}
           mode="dropdown"
@@ -102,7 +117,7 @@ export default function DatePicker({ label, onChange, value }) {
         </Picker>
         <Picker
           // id="year"
-          selectedValue={internalValue.dobYear}
+          selectedValue={internalYear}
           onValueChange={handleChange('dobYear')}
           style={{ width: '30%' }}
           mode="dropdown"
